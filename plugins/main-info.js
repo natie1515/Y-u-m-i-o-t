59532991b228379202de2d24782c4bd56310beec
natie1515 <frejulian57@gmail.com>
2026-01-11 21:39:15 -0400

    Change API endpoint for fetching repository data

--- plugins/main-info.js
+++ plugins/main-info.js
@@ -87,7 +87,7 @@ break
 }
 case 'script': case 'sc': {
 await m.react('🕒')
-const res = await fetch('https://api.github.com/repos/shadox-xyz/YuzukiBot-MD')
+const res = await fetch('https://whatsapp.com/channel/0029VaCDajZ9WtBvBZy76k2h')
 if (!res.ok) throw new Error('No se pudo obtener los datos del repositorio.')
 const json = await res.json()
 const txt = `*乂  S C R I P T  -  M A I N  乂*\n\n✩ *Nombre* : ${json.name}\n✩ *Visitas* : ${json.watchers_count}\n✩ *Peso* : ${(json.size / 1024).toFixed(2)} MB\n✩ *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n✩ *Url* : ${json.html_url}\n✩ *Forks* : ${json.forks_count}\n✩ *Stars* : ${json.stargazers_count}\n\n> *${dev}*`
